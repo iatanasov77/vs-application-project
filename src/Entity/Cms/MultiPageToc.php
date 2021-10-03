075856f84b08b8b85fb881f3ddee79a1d6f45a83
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-10-03 13:07:01 +0000

    Add Missing Entities for Cms Bundle.

---
+++ src/Entity/Cms/MultiPageToc.php
@@ -0,0 +1,15 @@
+<?php namespace App\Entity\Cms;
+
+use Doctrine\ORM\Mapping as ORM;
+use VS\CmsBundle\Model\MultiPageToc as BaseMultiPageToc;
+
+/**
+ * MultiPageToc
+ *
+ * @ORM\Table(name="VSCMS_MultiPageToc")
+ * @ORM\Entity
+ */
+class MultiPageToc extends BaseMultiPageToc
+{
+    
+}
