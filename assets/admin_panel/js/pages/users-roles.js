4d72250476dc50233c73f124ef1b136f2d638122
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-07 13:16:04 +0000

    Many Fixes, Additions and Refactoring.

--- assets/admin_panel/js/pages/users-roles.js
+++
@@ -1,8 +0,0 @@
-require( '@kanety/jquery-simple-tree-table/dist/jquery-simple-tree-table.js' );
-require( 'jquery-easyui/css/easyui.css' );
-require( 'jquery-easyui/js/jquery.easyui.min.js' );
-
-$( function()
-{
-
-});
