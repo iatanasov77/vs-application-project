1b12e95671e9c9579b72408284cb5326cd4a5ed3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-20 09:48:20 +0000

    Fix Asset.

--- assets/admin-panel/js/pages/filemanager-index.js
+++ assets/admin-panel/js/pages/filemanager-index.js
@@ -53,25 +53,3 @@ $( function()
         fileInput.setUploadFile( $( this ) );
     })
 });
-require( '../includes/resource-delete.js' );
-//require ( 'jquery-duplicate-fields/jquery.duplicateFields.js' );
-
-$( function()
-{
-    $( '.btnUploadFiles' ).on( 'click', function()
-    {
-        $.ajax({
-            type: "GET",
-            url: $( this ).attr( 'data-url' ),
-            success: function( response )
-            {
-                $( '#cardUploadFile > div.card-body' ).html( response );
-                $( '#modalUploadFile' ).modal( 'toggle' );
-            },
-            error: function()
-            {
-                alert( "SYSTEM ERROR!!!" );
-            }
-        });
-    });
-});
