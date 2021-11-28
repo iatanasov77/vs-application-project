d5df35bcca32b52a417f28bc2ad144b656911ee3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-11-28 12:43:58 +0000

    Improve Admin Panel Assets.

--- assets/admin-panel/js/pages/users-edit.js
+++ assets/admin-panel/js/pages/users-edit.js
@@ -10,5 +10,19 @@ $( function()
 		$( '#user_form_password_first' ).removeAttr( "required" );
 		$( '#user_form_password_second' ).removeAttr( "required" );
 	}
+	
+	/**
+     * Submit Checked Roles Tree
+     */
+	$( 'form[name="user_form"]' ).on( 'submit', function ( e )
+	{
+        let treeModule  = require( '../includes/tree.js' );
+        var element = treeModule.createCheckedTreeElement(
+            "selectedRoles",
+            $( '#user_form_roles_options' ).combotree( 'tree' ).tree( 'getChecked' )
+        );
+        $( this ).append( element );
+	});
+	
 });
  
