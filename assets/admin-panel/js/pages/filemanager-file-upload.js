f8a7925f646496d40bd5d2ab153f3dcdfaf6f8c4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-11-28 05:47:09 +0000

    Set file name in profile picture file input.

--- assets/admin-panel/js/pages/filemanager-file-upload.js
+++ assets/admin-panel/js/pages/filemanager-file-upload.js
@@ -1,6 +1,11 @@
-require( '../includes/file-input.js' );
+import { getFileName } from '../includes/file-input.js';
 
 $( function()
 {
-    
+    $( '#profile_form_profilePicture' ).on( 'change', function( e )
+    {
+        var newFilename = getFileName( $( this ) );
+        
+        $( '#profilePictureName' ).text( newFilename );
+    });
 });
