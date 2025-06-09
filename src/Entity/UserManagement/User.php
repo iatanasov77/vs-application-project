a20f1d00236051943f1eb5cc2fa61ce681c4f97c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-06-09 08:34:28 +0000

    Update to Vankosoft Core 1.13

--- src/Entity/UserManagement/User.php
+++ src/Entity/UserManagement/User.php
@@ -3,6 +3,10 @@
 use Doctrine\ORM\Mapping as ORM;
 use Vankosoft\UsersBundle\Model\User as BaseUser;
 
+/**
+ * @Doctrine\Common\Annotations\Annotation\IgnoreAnnotation( "ORM\MappedSuperclass" )
+ * @Doctrine\Common\Annotations\Annotation\IgnoreAnnotation("ORM\Column")
+ */
 #[ORM\Entity]
 #[ORM\Table(name: "VSUM_Users")]
 class User extends BaseUser
