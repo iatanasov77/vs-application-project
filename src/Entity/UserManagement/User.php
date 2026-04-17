93e050210e439d6030e4604ac1a37b8cc381d527
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-04-17 15:42:48 +0000

    Remove Annotations from Entites. Allready NOT Needed.

--- src/Entity/UserManagement/User.php
+++ src/Entity/UserManagement/User.php
@@ -3,10 +3,6 @@
 use Doctrine\ORM\Mapping as ORM;
 use Vankosoft\UsersBundle\Model\User as BaseUser;
 
-/**
- * @Doctrine\Common\Annotations\Annotation\IgnoreAnnotation( "ORM\MappedSuperclass" )
- * @Doctrine\Common\Annotations\Annotation\IgnoreAnnotation("ORM\Column")
- */
 #[ORM\Entity]
 #[ORM\Table(name: "VSUM_Users")]
 class User extends BaseUser
